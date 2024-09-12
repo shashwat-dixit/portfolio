81ca8dfe3d62b66e19100b1aca4a23e902cd192c
Shashwat Dixit <shashwatmain@gmail.com>
2024-09-12 19:36:47 +0530

    Init

---
+++ app/blog/layout.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BlogLayout() {
+  return (
+    <div>Blog Layout</div>
+  )
+}
---
+++ app/hire/page.tsx
@@ -0,0 +1,10 @@
+import ContactForm from '@/components/hire/ContactForm'
+import React from 'react'
+
+export default function HirePage() {
+  return (
+    <div>
+      <ContactForm />
+    </div>
+  )
+}
