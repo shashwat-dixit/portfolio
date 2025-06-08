e0ead32d35d5569f4a14ef51ceb319863c39256e
Shashwat Dixit <shashwatmain@gmail.com>
2025-06-08 16:54:46 +0530

    Added sentry and posthog

--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,8 @@ go 1.24.2
 
 require (
 	github.com/getsentry/sentry-go v0.33.0 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/posthog/posthog-go v1.5.11 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
---
+++ next-app/src/instrumentation-client.ts
@@ -0,0 +1,10 @@
+import posthog from "posthog-js"
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: "/ingest",
+  ui_host: "https://us.posthog.com",
+  capture_pageview: 'history_change',
+  capture_pageleave: true,
+  capture_exceptions: true,
+  debug: process.env.NODE_ENV === "development",
+});
