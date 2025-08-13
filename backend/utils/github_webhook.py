1d951b4cd22a75258007e07625a46f1de11449a2
Shashwat Dixit <shashwatmain@gmail.com>
2025-08-14 00:11:22 +0530

    Update

--- backend/utils/github_webhook.py
+++ backend/utils/github_webhook.py
@@ -1,2 +1,3 @@
 # Define github webhooks for fetching files on commit.
-# Find a way to fetch only files that have been changed
+# Need to find a way to index files once and then only re-index those files
+# which are being changed in a commit.
