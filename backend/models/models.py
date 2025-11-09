372871905dab978c14c50a00fe9de6f07d5403dc
Shashwat Dixit <shashwatmain@gmail.com>
2025-11-09 23:55:12 +0530

    Fix models and add support for SSR

--- backend/models/models.py
+++ backend/models/models.py
@@ -1,35 +1,37 @@
-from pydantic import BaseModel, JsonValue
-from datetime import datetime
-from uuid import UUID
-from typing import List
+from datetime import date
+from uuid import UUID, uuid4
+from typing import Any
+from sqlmodel import SQLModel, Field, Column, Relationship
+from sqlalchemy.dialects.postgresql import JSONB
 
 
-class TagRead(BaseModel):
-    id: int
-    name: str
+class BlogPostTagLink(SQLModel, table=True):
+    post_id: UUID = Field(foreign_key="blog.id", primary_key=True)
+    tag_id: UUID = Field(foreign_key="tag.id", primary_key=True)
 
 
-class BlogListItem(BaseModel):
-    id: UUID
+class Tag(SQLModel, table=True):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    name: str = Field(unique=True, index=True)
+
+    posts: list["Blog"] = Relationship(back_populates="tags", link_model=BlogPostTagLink)
+
+
+class Blog(SQLModel, table=True):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
     title: str
-    date: datetime
-    slug: str
+    slug: str = Field(index=True, unique=True)
     description: str
-    banner: str
-    tags: List[TagRead]
-
-
-class BlogDetail(BaseModel):
-    id: UUID
-    title: str
-    date: datetime
-    modified_at: datetime
-    author: str
-    slug: str
-    description: str
-    banner: str
-    tags: List[TagRead]
-    seo: JsonValue
-    openGraph: JsonValue
-    twitter: JsonValue
-    content_html: str
+    author: str = Field(default="Shashwat Dixit")
+    published_date: date | None = Field(default=None)
+    last_modified_date: date | None = Field(default=None)
+    image: str | None = None
+    seo: dict[str, Any] | None = Field(default=None, sa_column=Column(JSONB))
+    openGraph: dict[str, Any] | None = Field(default=None, sa_column=Column(JSONB))
+    twitter: dict[str, Any] | None = Field(default=None, sa_column=Column(JSONB))
+    content: str
+    tags: list[Tag] = Relationship(back_populates="posts", link_model=BlogPostTagLink)
+    file_hash: str = Field(nullable=False, index=True)
+    # Not doing this now, if needed will add it if perf is not satisfactory.
+    # Stores Pre-rendered HTML
+    # content_html: str | None = None
--- backend/utils/file_services.py
+++ backend/utils/file_services.py
@@ -6,13 +6,9 @@ def generate_checksum(file_name: str) -> str:
         file_hash = hashlib.file_digest(file, 'sha256').hexdigest()
     return file_hash
 
-def verify_checksum(file_name: str) -> bool:
+def verify_checksum(file_name: str, slug: str) -> bool:
     file_hash = generate_checksum(file_name)
-    statement = select(Blog.file_hash)where(Blog.file_name == file_name)
-
+    statement = select(Blog.file_hash)where(Blog.slug == slug)
     db_file_hash = session.exec(statement).first()
 
-    if db_file_hash == file_hash:
-        return True
-    
-    return False
+    return db_file_hash == file_hash
